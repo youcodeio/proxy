17da6d0cb4d895acdb31c3532e816a63430b34bf
PierreZ <pierre.zemb.isen@gmail.com>
2016-03-28 15:27:18 +0200

    adding some tests

--- handlers.go
+++ handlers.go
@@ -13,11 +13,17 @@ import (
 func GetQuery(w http.ResponseWriter, r *http.Request) {
 
 	query := r.URL.Query().Get("query")
+	if len(query) == 0 {
+		http.Error(w, "Not enough args", http.StatusBadRequest)
+		return
+	}
+
 	channels, err := getChannels()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	resultsChannel := make(chan []youtube.SearchResultSnippet, MaxResults)
 
 	var wg sync.WaitGroup
